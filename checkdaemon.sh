d85c757909390437e159a82200ca66e136745796
msg768 <markus.ghambari@gmail.com>
2024-01-25 21:46:44 +1100

    Update checkdaemon.sh

--- checkdaemon.sh
+++ checkdaemon.sh
@@ -2,7 +2,7 @@
 # checkdaemon.sh
 # Make sure the daemon is not stuck.
 # Add the following to the crontab (i.e. crontab -e)
-# */120 * * * * ~/swiftnode/checkdaemon.sh
+# */180 * * * * ~/swiftnode/checkdaemon.sh
 
 previousBlock=$(cat ~/swiftnode/blockcount)
 currentBlock=$(swiftcash-cli getblockcount)
